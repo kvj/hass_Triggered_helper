2230cc7a4785ef19aa2ba7ac34b623f7d7f63e95
Konstantin Vorobyev <konstantin.vorobyev@zalando.de>
2024-08-05 17:51:34 +0200

    Fix config data update handling

--- custom_components/triggered_sensor/__init__.py
+++ custom_components/triggered_sensor/__init__.py
@@ -10,6 +10,8 @@ import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 import logging
 
+import traceback
+
 _LOGGER = logging.getLogger(__name__)
 
 CONFIG_SCHEMA = vol.Schema({
@@ -19,10 +21,12 @@ CONFIG_SCHEMA = vol.Schema({
 
 async def _async_update_entry(hass, entry):
     _LOGGER.debug(f"_async_update_entry: {entry}")
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
+    coordinator = hass.data[DOMAIN]["devices"][entry.entry_id]
+    await coordinator.async_unload()
+    await coordinator.async_load()
 
 async def async_setup_entry(hass: HomeAssistant, entry):
+    _LOGGER.debug(f"async_setup_entry: {entry}")
     data = entry.as_dict()["data"]
 
     platform = hass.data[DOMAIN]["platform"]
@@ -32,16 +36,13 @@ async def async_setup_entry(hass: HomeAssistant, entry):
     await coordinator.async_config_entry_first_refresh()
     await coordinator.async_load()
 
-    for p in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, p)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry):
+    _LOGGER.debug(f"async_unload_entry: {entry}")
     coordinator = hass.data[DOMAIN]["devices"][entry.entry_id]
-    for p in PLATFORMS:
-        await hass.config_entries.async_forward_entry_unload(entry, p)
+    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     await coordinator.async_unload()
     hass.data[DOMAIN]["devices"].pop(entry.entry_id)
     return True
